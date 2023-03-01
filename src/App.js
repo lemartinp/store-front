d9cec3e7d55a2ad7ec0bc3bfddfd86384099630b
lemartinp <lemartinp@unal.edu.co>
2023-03-01 08:12:46 -0500

    components and pages done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 //import { ArrowRightOutlined } from '@mui/icons-material';
 import './App.css';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
-  return <Home/>;
+  return <Cart/>;
 }
 
 export default App;
